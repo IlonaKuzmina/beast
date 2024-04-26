927048a7c4d38796d2e2a34053863183250a72a9
IlonaKuzmina-GWM <ilona@greenwiremedia.com>
2024-04-26 09:48:57 +0300

    FAQfield, opne/close functionaliti added

--- src/components/FAQ/FAQ.tsx
+++ src/components/FAQ/FAQ.tsx
@@ -1,49 +1,47 @@
-import React from 'react'
+'use client'
+
+import React, { useState } from 'react'
+import { FAQfield } from './FAQfield';
 
 export const FAQ = () => {
+    const [isOpenIndex, setIsOpenIndex] = useState<number | null>(null);
+
+    const content = [
+        {
+            question: "How to buy crypto with credit card?",
+            answer: "The process is very easy! So, how does it work? To buy crypto with credit card, you should register an account, select a cryptocurrency you’d like to buy, make a payment, and receive crypto! Crypto purchases up to $700 require only basic verification, which is a perfect option for crypto enthusiasts."
+        },
+        {
+            question: "Where to buy cryptocurrency with credit card?",
+            answer: "We offer the fastest and easiest way to buy crypto with credit card. The key step is to go through the registration process, which won’t take long. Worry about our level of security? We use the 3D secure protocol to ensure first-class security for your debit and credit card payments. Thus, be sure your assets are protected with high-end encryption technology so that you can buy crypto with credit card securely."
+        },
+        {
+            question: "What is the best place to buy crypto?",
+            answer: "If you’re still looking for the best place to buy crypto, you’re definitely in the right place! We’re sure that Denrosa service is the best place to buy and sell crypto since it’s a licensed financial services provider with guaranteed legal compliance and a secure infrastructure system for fast crypto exchange services at fair prices. In addition to traditional purchases with a bank card, you can use alternative payment methods available (for instance, SEPA bank transfers)."
+        },
+        {
+            question: "What is the best way to buy crypto with debit or credit card?",
+            answer: "Denrosa site offers the best way to buy crypto and all leading altcoins using any credit, prepaid, or debit card issued by VISA, Mastercard, or Maestro. All crypto exchanges are characterized by fast speed and maximum efficiency — you can enjoy the best way to buy crypto online at the best exchange rates and without any extra charges."
+        },
+        {
+            question: "How to sell crypto online?",
+            answer: "It’s easy and simple with Denrosa! We’re the most convenient place to sell crypto for EUR. All you need to do is select an amount of cryptocurrency for sale, fill in your SEPA account details, transfer crypto and receive EUR to your SEPA account! Also, you have an awesome opportunity to sell crypto online for your local fiat currencies. Sell and buy crypto online at Denrosa easily and securely!"
+        }
+    ];
+
+    const answerToggler = (index: number) => {
+        const isItemAlreadyOpen = isOpenIndex === index;
+        setIsOpenIndex(isItemAlreadyOpen ? null : index);
+    }
+
     return (
         <section className=" py-[150px] text-lightText bg-backgroundSecondary w-full">
             <div className="container">
                 <h2 className="mb-[60px] text-[45px] font-bold text-center">FAQ</h2>
-                <div className="bg-backgroundBox rounded-lg p-[40px] rounded-xl mb-[20px]">
-                    <div className="text-[20px] font-bold flex flex-row justify-between items-center">
-                        How to buy crypto with credit card?
-                        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="19" viewBox="0 0 18 19" fill="none">
-                            <rect x="18" y="7.71436" width="4" height="18" transform="rotate(90 18 7.71436)" fill="#ffffff"></rect>
-                        </svg>
-                    </div>
-                    <div className="pt-[40px] max-w-[90%]">
-                        The process is very easy! So, how does it work? To buy crypto with credit card, you should register an account, select a cryptocurrency you’d like to buy, make a payment, and receive crypto! Crypto purchases up to $700 require only basic verification, which is a perfect option for crypto enthusiasts.
-                        The process is very easy! So, how does it work? To buy crypto with credit card, you should register an account, select a cryptocurrency you’d like to buy, make a payment, and receive crypto! Crypto purchases up to $700 require only basic verification, which is a perfect option for crypto enthusiasts.
-                    </div>
-                </div>
-                <div className="bg-backgroundBox rounded-lg p-[40px] rounded-xl mb-[20px]">
-                    <div className="text-[20px] font-bold flex flex-row justify-between items-center">
-                        What is the best way to buy crypto with debit or credit card? 
-                        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="19" viewBox="0 0 18 19" fill="none">
-                            <rect x="7" y="0.714355" width="4" height="18" fill="#ffffff"></rect>
-                            <rect x="18" y="7.71436" width="4" height="18" transform="rotate(90 18 7.71436)" fill="#ffffff"></rect>
-                        </svg>
-                    </div>
-                </div>
-                <div className="bg-backgroundBox rounded-lg p-[40px] rounded-xl mb-[20px]">
-                    <div className="text-[20px] font-bold flex flex-row justify-between items-center">
-                        What is the best way to buy crypto with debit or credit card? 
-                        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="19" viewBox="0 0 18 19" fill="none">
-                            <rect x="7" y="0.714355" width="4" height="18" fill="#ffffff"></rect>
-                            <rect x="18" y="7.71436" width="4" height="18" transform="rotate(90 18 7.71436)" fill="#ffffff"></rect>
-                        </svg>
-                    </div>
-                </div>
-                <div className="bg-backgroundBox rounded-lg p-[40px] rounded-xl mb-[20px]">
-                    <div className="text-[20px] font-bold flex flex-row justify-between items-center">
-                        What is the best way to buy crypto with debit or credit card? 
-                        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="19" viewBox="0 0 18 19" fill="none">
-                            <rect x="7" y="0.714355" width="4" height="18" fill="#ffffff"></rect>
-                            <rect x="18" y="7.71436" width="4" height="18" transform="rotate(90 18 7.71436)" fill="#ffffff"></rect>
-                        </svg>
-                    </div>
-                </div>
+
+                {content.map((item, index) => (
+                    <FAQfield key={index} isOpen={isOpenIndex === index} question={item.question} answer={item.answer} answerToggler={() => answerToggler(index)} />
+                ))}
             </div>
         </section>
     )
---
+++ src/components/FAQ/FAQfield.tsx
@@ -0,0 +1,41 @@
+
+import { Transition } from '@windmill/react-ui'
+
+interface IFAQfield {
+    isOpen: boolean
+    question: string
+    answer: string
+    answerToggler: () => void
+}
+
+export const FAQfield = ({ isOpen, question, answer, answerToggler }: IFAQfield) => {
+    return (
+        <div className="bg-backgroundBox p-[40px] rounded-xl mb-[20px] cursor-pointer">
+            <div className="text-[20px] font-bold flex flex-row justify-between items-center" onClick={answerToggler}>
+                {question}
+
+                {!isOpen
+                    ? <svg xmlns="http://www.w3.org/2000/svg" width="18" height="19" viewBox="0 0 18 19" fill="none">
+                        <rect x="7" y="0.714355" width="4" height="18" fill="#ffffff"></rect>
+                        <rect x="18" y="7.71436" width="4" height="18" transform="rotate(90 18 7.71436)" fill="#ffffff"></rect>
+                    </svg> : <svg xmlns="http://www.w3.org/2000/svg" width="18" height="19" viewBox="0 0 18 19" fill="none">
+                        <rect x="18" y="7.71436" width="4" height="18" transform="rotate(90 18 7.71436)" fill="#ffffff"></rect>
+                    </svg>
+                }
+            </div>
+            <Transition
+                show={isOpen}
+                enter="transition ease-out duration-500 transform"
+                enterFrom="opacity-0 scale-95"
+                enterTo="opacity-100 scale-100"
+                leave="transition ease-in duration-500 transform"
+                leaveFrom="opacity-100 scale-100"
+                leaveTo="opacity-0 scale-95"
+            >
+                <div className="pt-[40px] max-w-[90%]">
+                    {answer}
+                </div>
+            </Transition>
+        </div>
+    )
+}
